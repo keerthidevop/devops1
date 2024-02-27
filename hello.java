e477e0e31abab59cc2be73a15c1245fbdee347e9
dev1 <dev1@mail.com>
2024-02-27 22:21:42 +0530

    mod by dev1

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
-class hello 
+class hello  #from dev1pract1 
 {
- public static void main()
+ public static void main() #from dev1pract1
  {
-	 system.out.print("hello");
+	 system.out.print("hello from dev1");
  }
 }
 
